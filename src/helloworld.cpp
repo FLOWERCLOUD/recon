02706bd74970242d3b54f91517334736de3fc4c8
David Lin <davll.xc@gmail.com>
2015-05-04 14:48:26 +0800

    minor fix: viewport

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -108,6 +108,8 @@ void BundleWindow::render()
   using vectormath::aos::store_mat3;
   using vectormath::aos::store_mat4;
 
+  glViewport(0, 0, width(), height());
+
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
