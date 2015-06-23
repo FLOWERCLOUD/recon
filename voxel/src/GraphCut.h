705852c954465b7b822d471041c43f433e1b8944
David Lin <davll.xc@gmail.com>
2015-06-23 21:29:10 +0800

    add paper

--- voxel/src/GraphCut.h
+++ voxel/src/GraphCut.h
@@ -13,6 +13,10 @@
   Multi-view Stereo via Volumetric Graph-cuts
   George Vogiatzis, P.H.S. Torr, R. Cipolla
   CVPR 2005
+
+  Automatic 3D object segmentation in multiple views using volumetric graph-cuts
+  N.D.F. Campbell, G. Vogiatzis, C. Hernandez and R. Cipolla
+  Image and Vision Computing 2010
 */
 
 // N.D.F. Cambell, G. Vogiatzis, C. Hernández and R. Cipolla, CVMP 2011. Automatic Object Segmentation from Calibrated Images
