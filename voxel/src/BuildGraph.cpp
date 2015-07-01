d5a420055047ccc44723ec21467020e43db0b558
David Lin <davll.xc@gmail.com>
2015-07-01 21:23:46 +0800

    optimize

--- voxel/src/BuildGraph.cpp
+++ voxel/src/BuildGraph.cpp
@@ -37,22 +37,6 @@ struct PhotoConsistency {
   {
   }
 
-  QList<int> closest_cameras(int cam_i, Point3 x)
-  {
-    QList<int> jcams;
-    Camera ci = cameras[cam_i];
-    Vec3 ni = normalize(ci.center() - x);
-    for (int j = 0, n = cameras.size(); j < n; ++j) {
-      Camera cj = cameras[j];
-      Vec3 nj = normalize(cj.center() - x);
-      float dp = (float)dot(ni, nj);
-      if (dp <= 0.984807753012208f && dp >= 0.9396926207859084f) {
-        jcams.append(j);
-      }
-    }
-    return jcams;
-  }
-
   QImage image(int cam_id)
   {
     if (images[cam_id].isNull()) {
@@ -61,18 +45,12 @@ struct PhotoConsistency {
     return images[cam_id];
   }
 
-  SampleWindow sample(int cam_i, Point3 pos)
-  {
-    Camera cam = cameras[cam_i];
-    QImage img = image(cam_i);
-    Mat4 txfm = cam.intrinsicForImage(img.width(), img.height());
-    txfm = txfm * cam.extrinsic();
-    return SampleWindow(img, Vec3::proj(transform(txfm, pos)));
-  }
-
   struct VoxelScore {
-    QList<QImage> image_js;
-    QList<Mat4> txfm_js;
+    static const int MAX_J = 4;
+    int num_j;
+    int cam_js[MAX_J];
+    QImage image_js[MAX_J];
+    Mat4 txfm_js[MAX_J];
     Ray3 o;
     SampleWindow sw_i;
 
@@ -83,8 +61,8 @@ struct PhotoConsistency {
     , sw_i()
     {
       {
-        Camera ci = pc->cameras[cam_i];
-        QImage image_i = pc->image(cam_i);
+        const Camera& ci = pc->cameras[cam_i];
+        const QImage& image_i = pc->image(cam_i);
         int w = image_i.width(), h = image_i.height();
         Mat4 txfm_i = ci.intrinsicForImage(w, h) * ci.extrinsic();
         sw_i = SampleWindow(image_i, Vec3::proj(transform(txfm_i, x)));
@@ -94,22 +72,46 @@ struct PhotoConsistency {
         o = Ray3(x, dir);
       }
 
-      for (int cam_j : pc->closest_cameras(cam_i, x)) {
-        Camera cj = pc->cameras[cam_j];
-        QImage image_j = pc->image(cam_j);
+      closest_cameras(pc->cameras, cam_i, x);
+
+      for (int i = 0; i < num_j; ++i) {
+        int cam_j = cam_js[i];
+        const Camera& cj = pc->cameras[cam_j];
+        const QImage& image_j = pc->image(cam_j);
         int w = image_j.width(), h = image_j.height();
         Mat4 txfm_j = cj.intrinsicForImage(w, h) * cj.extrinsic();
-        image_js.append(image_j);
-        txfm_js.append(txfm_j);
+        image_js[i] = image_j;
+        txfm_js[i] = txfm_j;
       }
+    }
+
+    void closest_cameras(const QList<Camera>& cameras, int cam_i, Point3 x)
+    {
+      int count = 0;
+      const Camera& ci = cameras[cam_i];
+      Vec3 ni = normalize(ci.center() - x);
+
+      for (int j = 0, n = cameras.size(); j < n; ++j) {
+        const Camera& cj = cameras[j];
+        Vec3 nj = normalize(cj.center() - x);
+
+        float dp = (float)dot(ni, nj);
+        if (dp <= 0.984807753012208f && dp >= 0.9396926207859084f) {
+          cam_js[count++] = j;
+          if (count >= MAX_J)
+            break;
+        }
+      }
+
+      num_j = count;
     }
 
     inline double operator()(float d) const
     {
-      int nj = image_js.size();
+      int nj = num_j;
       double sum = 0.0;
       for (int j = 0; j < nj; ++j) {
-        QImage image_j = image_js[j];
+        const QImage& image_j = image_js[j];
         Mat4 txfm_j = txfm_js[j];
         // Compute NCC
         SampleWindow sw_j(image_j, Vec3::proj(transform(txfm_j, o[d])));
@@ -122,6 +124,7 @@ struct PhotoConsistency {
   double vote(int cam_i, Point3 pos)
   {
     static const float drange[] = {
+#if 0
       -5.0f,-4.9f,-4.8f,-4.7f,-4.6f,-4.5f,-4.4f,-4.3f,-4.2f,-4.1f,
       -4.0f,-3.9f,-3.8f,-3.7f,-3.6f,-3.5f,-3.4f,-3.3f,-3.2f,-3.1f,
       -3.0f,-2.9f,-2.8f,-2.7f,-2.6f,-2.5f,-2.4f,-2.3f,-2.2f,-2.1f,
@@ -133,6 +136,11 @@ struct PhotoConsistency {
       3.0f, 3.1f, 3.2f, 3.3f, 3.4f, 3.5f, 3.6f, 3.7f, 3.8f, 3.9f,
       4.0f, 4.1f, 4.2f, 4.3f, 4.4f, 4.5f, 4.6f, 4.7f, 4.8f, 4.9f,
       5.0f
+#else
+      -5.0f,-4.5f,-4.0f,-3.5f,-3.0f,-2.5f,-2.0f,-1.5f,-1.0f,-0.5f,
+      0.0f, 0.5f, 1.0f, 1.5f, 2.0f, 2.5f, 3.0f, 3.5f, 4.0f, 4.5f,
+      5.0f
+#endif
     };
     static const int dsamples = sizeof(drange) / sizeof(float);
 
@@ -208,7 +216,7 @@ void build_graph(VoxelGraph& graph,
       AABox vbox = model.element_box(m);
       Vec3 center = (Vec3)vbox.center();
       Vec3 minpos = (Vec3)vbox.minpos;
-      printf("current voxel = %d %d %d\n", x, y, z);
+      //printf("current voxel = %d %d %d\n", x, y, z);
 
       if (x > 0) {
         double v = pc.total_votes((Point3)copy_x(center, minpos));
