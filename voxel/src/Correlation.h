db16964843284e5d31ff0e47349ba81ac26ff5e7
David Lin <davll.xc@gmail.com>
2015-07-02 20:13:25 +0800

    speed up a little

--- voxel/src/Correlation.h
+++ voxel/src/Correlation.h
@@ -4,6 +4,7 @@
 #include <vectormath/aos/utils/point3.h>
 #include <stdint.h>
 #include <math.h>
+#include <algorithm>
 #include <QImage>
 
 namespace recon {
@@ -69,47 +70,6 @@ struct SampleWindow {
     return (1.0f - fy) * v_0 + fy * v_1;
   }
 
-  inline void set_bilinear(const QImage& image, Vec3 xy)
-  {
-    int width = image.width(), height = image.height();
-
-    float ix, iy;
-    float fx = modff((float)xy.x(), &ix);
-    float fy = modff((float)xy.y(), &iy);
-
-    int px = (int)ix, py = (int)iy;
-    if (px >= 0 && py >= 0 && px < width && py < height) {
-      uint8_t r[12][12];
-      uint8_t g[12][12];
-      uint8_t b[12][12];
-
-      for (int i = 0; i < 12; ++i) {
-        for (int j = 0; j < 12; ++j) {
-          int x = px - 5 + j, y = py - 5 + i;
-          x = (x < 0 ? 0 : x);
-          x = (x < width ? x : width-1);
-          y = (y < 0 ? 0 : y);
-          y = (y < height ? y : height-1);
-          QRgb c = image.pixel(x, y);
-          r[i][j] = qRed(c);
-          g[i][j] = qGreen(c);
-          b[i][j] = qBlue(c);
-        }
-      }
-
-      for (int i = 0; i < 11; ++i) {
-        for (int j = 0; j < 11; ++j) {
-          int index = i*11+j;
-          float cr = bilinear(fx, fy, r[i][j], r[i][j+1], r[i+1][j], r[i+1][j+1]);
-          float cg = bilinear(fx, fy, g[i][j], g[i][j+1], g[i+1][j], g[i+1][j+1]);
-          float cb = bilinear(fx, fy, b[i][j], b[i][j+1], b[i+1][j], b[i+1][j+1]);
-          color[index] = Vec3(cr, cg, cb);
-        }
-      }
-      valid = true;
-    }
-  }
-
   inline void set_bilinear_rgb32(const QImage& image, Vec3 xy)
   {
     Q_ASSERT(image.format() == QImage::Format_RGB32);
@@ -122,34 +82,25 @@ struct SampleWindow {
 
     int px = (int)ix, py = (int)iy;
     if (px >= 0 && py >= 0 && px < width && py < height) {
-      uint8_t r[12][12];
-      uint8_t g[12][12];
-      uint8_t b[12][12];
-
-      for (int i = 0; i < 12; ++i) {
-        int y = py - 5 + i;
-        y = (y < 0 ? 0 : y);
-        y = (y < height ? y : height-1);
-        const QRgb* row = (const QRgb*)image.constScanLine(y);
-
-        for (int j = 0; j < 12; ++j) {
-          int x = px - 5 + j;
-          x = (x < 0 ? 0 : x);
-          x = (x < width ? x : width-1);
-          QRgb c = row[x];
-          r[i][j] = qRed(c);
-          g[i][j] = qGreen(c);
-          b[i][j] = qBlue(c);
-        }
-      }
-
       for (int i = 0; i < 11; ++i) {
+        int y0 = py - 5 + i, y1 = py - 4 + i;
+        y0 = std::min(std::max(y0, 0), height-1);
+        y1 = std::min(std::max(y1, 0), height-1);
+        const QRgb* row0 = (const QRgb*)image.constScanLine(y0);
+        const QRgb* row1 = (const QRgb*)image.constScanLine(y1);
+
         for (int j = 0; j < 11; ++j) {
-          int index = i*11+j;
-          float cr = bilinear(fx, fy, r[i][j], r[i][j+1], r[i+1][j], r[i+1][j+1]);
-          float cg = bilinear(fx, fy, g[i][j], g[i][j+1], g[i+1][j], g[i+1][j+1]);
-          float cb = bilinear(fx, fy, b[i][j], b[i][j+1], b[i+1][j], b[i+1][j+1]);
-          color[index] = Vec3(cr, cg, cb);
+          int x0 = px - 5 + j, x1 = px - 4 + j;
+          x0 = std::min(std::max(x0, 0), width-1);
+          x1 = std::min(std::max(x1, 0), width-1);
+          QRgb c0 = row0[x0];
+          QRgb c1 = row0[x1];
+          QRgb c2 = row1[x0];
+          QRgb c3 = row1[x1];
+          float cr = bilinear(fx, fy, qRed(c0), qRed(c1), qRed(c2), qRed(c3));
+          float cg = bilinear(fx, fy, qGreen(c0), qGreen(c1), qGreen(c2), qGreen(c3));
+          float cb = bilinear(fx, fy, qBlue(c0), qBlue(c1), qBlue(c2), qBlue(c3));
+          color[i*11+j] = Vec3(cr, cg, cb);
         }
       }
       valid = true;
---
+++ voxel/tools/planexz.py
@@ -0,0 +1,14 @@
+def parse_args():
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="Visualize Plane XZ of Graph")
+    parser.add_argument('graph', help='Input Graph data file')
+    parser.add_argument('output', help='Output Image file')
+    parser.add_argument('--plane-y', type=int)
+    return parser.parse_args()
+
+def mainfunc():
+    global ARGS
+    ARGS = parse_args()
+
+if __name__ == "__main__":
+    mainfunc()
