74b0b81cc3b7cb528a8bd6549c0d2b2469791575
David Lin <davll.xc@gmail.com>
2015-07-02 14:42:07 +0800

    try optimizing

--- voxel/src/Correlation.h
+++ voxel/src/Correlation.h
@@ -14,9 +14,7 @@ using vectormath::aos::utils::Point3;
 
 struct SampleWindow {
   bool valid;
-  float red[121];
-  float green[121];
-  float blue[121];
+  Vec3 color[121];
 
   SampleWindow()
   : valid(false)
@@ -26,20 +24,20 @@ struct SampleWindow {
   SampleWindow(const QImage& image, Vec3 xy)
   : SampleWindow()
   {
-    set_bilinear(image, xy);
+    set_bilinear_rgb32(image, xy);
+    //set_bilinear(image, xy);
     //set_floor(image, xy);
 
     // normalize pixel value to 0.0 - 1.0
     for (int i = 0; i < 121; ++i) {
-      red[i] /= 255.0f;
-      green[i] /= 255.0f;
-      blue[i] /= 255.0f;
+      color[i] = color[i] / 255.0f;
     }
   }
 
   inline Vec3 operator[](int i) const
   {
-    return Vec3{ red[i], green[i], blue[i] };
+    Q_ASSERT(i >= 0 && i < 121);
+    return color[i];
   }
 
   inline void set_floor(const QImage& image, Vec3 xy)
@@ -59,11 +57,7 @@ struct SampleWindow {
         y = (y < height ? y : height-1);
 
         QRgb c = image.pixel(x, y);
-
-        int index = (i) * 11 + (j);
-        red[ index ] = qRed(c);
-        green[ index ] = qGreen(c);
-        blue[ index ] = qBlue(c);
+        color[i*11+j] = Vec3((float)qRed(c), (float)qGreen(c), (float)qBlue(c));
       }
     }
   }
@@ -106,9 +100,56 @@ struct SampleWindow {
       for (int i = 0; i < 11; ++i) {
         for (int j = 0; j < 11; ++j) {
           int index = i*11+j;
-          red[index] = bilinear(fx, fy, r[i][j], r[i][j+1], r[i+1][j], r[i+1][j+1]);
-          green[index] = bilinear(fx, fy, g[i][j], g[i][j+1], g[i+1][j], g[i+1][j+1]);
-          blue[index] = bilinear(fx, fy, b[i][j], b[i][j+1], b[i+1][j], b[i+1][j+1]);
+          float cr = bilinear(fx, fy, r[i][j], r[i][j+1], r[i+1][j], r[i+1][j+1]);
+          float cg = bilinear(fx, fy, g[i][j], g[i][j+1], g[i+1][j], g[i+1][j+1]);
+          float cb = bilinear(fx, fy, b[i][j], b[i][j+1], b[i+1][j], b[i+1][j+1]);
+          color[index] = Vec3(cr, cg, cb);
+        }
+      }
+      valid = true;
+    }
+  }
+
+  inline void set_bilinear_rgb32(const QImage& image, Vec3 xy)
+  {
+    Q_ASSERT(image.format() == QImage::Format_RGB32);
+
+    int width = image.width(), height = image.height();
+
+    float ix, iy;
+    float fx = modff((float)xy.x(), &ix);
+    float fy = modff((float)xy.y(), &iy);
+
+    int px = (int)ix, py = (int)iy;
+    if (px >= 0 && py >= 0 && px < width && py < height) {
+      uint8_t r[12][12];
+      uint8_t g[12][12];
+      uint8_t b[12][12];
+
+      for (int i = 0; i < 12; ++i) {
+        int y = py - 5 + i;
+        y = (y < 0 ? 0 : y);
+        y = (y < height ? y : height-1);
+        const QRgb* row = (const QRgb*)image.constScanLine(y);
+
+        for (int j = 0; j < 12; ++j) {
+          int x = px - 5 + j;
+          x = (x < 0 ? 0 : x);
+          x = (x < width ? x : width-1);
+          QRgb c = row[x];
+          r[i][j] = qRed(c);
+          g[i][j] = qGreen(c);
+          b[i][j] = qBlue(c);
+        }
+      }
+
+      for (int i = 0; i < 11; ++i) {
+        for (int j = 0; j < 11; ++j) {
+          int index = i*11+j;
+          float cr = bilinear(fx, fy, r[i][j], r[i][j+1], r[i+1][j], r[i+1][j+1]);
+          float cg = bilinear(fx, fy, g[i][j], g[i][j+1], g[i+1][j], g[i+1][j+1]);
+          float cb = bilinear(fx, fy, b[i][j], b[i][j+1], b[i+1][j], b[i+1][j+1]);
+          color[index] = Vec3(cr, cg, cb);
         }
       }
       valid = true;
@@ -151,22 +192,78 @@ struct NormalizedCrossCorrelation {
     // compute variance
     float si = 0.0f, sj = 0.0f;
     for (int i = 0; i < 121; ++i) {
-      si += powf((yi[i] - ai), 2);
-      sj += powf((yj[i] - aj), 2);
+      float di = yi[i] - ai;
+      float dj = yj[i] - aj;
+      si += di * di;
+      sj += dj * dj;
     }
-    si = sqrtf(si);
-    sj = sqrtf(sj);
+    if (si == 0.000001f || sj == 0.000001f)
+      return -1.0f;
+    //si = sqrtf(si);
+    //sj = sqrtf(sj);
+    float denom = sqrtf(si * sj);
 
     // compute dot product of two normalized vector
     float ncc = 0.0f;
     for (int i = 0; i < 121; ++i) {
-      ncc += ((yi[i] - ai) / si) * ((yj[i] - aj) / sj);
+      float di = yi[i] - ai;
+      float dj = yj[i] - aj;
+      //ncc += (di / si) * (dj / sj);
+      ncc += di * dj / denom;
     }
 
-    if (isnan(ncc))
-      return -1.0f;
+    //if (isnan(ncc))
+    //  return -1.0f;
     return ncc;
   }
+
+  /*static float zncc_sse(const SampleWindow& wi, const SampleWindow& wj)
+  {
+    // convert RGB to GRAY
+    __m128 yi[121], yj[121];
+    for (int i = 0; i < 121; ++i)
+      yi[i] = dot(RGB_TO_GRAY, wi[i]).xmm_data;
+    for (int i = 0; i < 121; ++i)
+      yj[i] = dot(RGB_TO_GRAY, wj[i]).xmm_data;
+
+    // compute mean
+    __m128 ai = _mm_setzero_ps(), aj = _mm_setzero_ps();
+    for (int i = 0; i < 121; ++i)
+      ai = _mm_add_ss(ai, yi[i]);
+    for (int i = 0; i < 121; ++i)
+      ai = _mm_add_ss(ai, yi[i]);
+    __m128 denom = _mm_set_ss(121.0f);
+    ai = _mm_div_ss(ai, denom);
+    aj = _mm_div_ss(aj, denom);
+
+    // compute variance
+    __m128 si = _mm_setzero_ps(), sj = _mm_setzero_ps();
+    for (int i = 0; i < 121; ++i) {
+      __m128 d = _mm_sub_ss(yi[i], ai);
+      si = _mm_add_ss(_mm_mul_ss(d, d), si);
+    }
+    for (int i = 0; i < 121; ++i) {
+      __m128 d = _mm_sub_ss(yj[i], aj);
+      sj = _mm_add_ss(_mm_mul_ss(d, d), sj);
+    }
+    si = _mm_sqrt_ss(si);
+    sj = _mm_sqrt_ss(sj);
+
+    // compute dot product of two normalized vector
+    __m128 ncc = _mm_setzero_ps();
+    for (int i = 0; i < 121; ++i) {
+      __m128 di = _mm_sub_ss(yi[i], ai);
+      __m128 dj = _mm_sub_ss(yj[i], aj);
+      di = _mm_div_ss(di, si);
+      dj = _mm_div_ss(dj, sj);
+      ncc = _mm_add_ss(_mm_mul_ps(di, dj), ncc);
+    }
+
+    float result = _mm_cvtss_f32(ncc);
+    if (isnan(result))
+      return -1.0f;
+    return result;
+  }*/
 
   inline operator float() const
   {
