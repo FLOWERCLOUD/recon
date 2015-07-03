b193d4d99799573f5b73d7e58cebb53d3ccb1937
David Lin <davll.xc@gmail.com>
2015-07-03 22:03:11 +0800

    wip

--- voxel/src/PhotoConsistency.h
+++ voxel/src/PhotoConsistency.h
@@ -16,17 +16,17 @@ struct ClosestCameras {
   int cam_js[MAX_NUM];
   Mat4 txfm_i;
   Mat4 txfm_js[MAX_NUM];
-  const QList<Camera>* cameras;
-  const QList<QImage>* images;
+  const QList<Camera>* _cameras;
+  const QList<QImage>* _images;
 
   ClosestCameras(const QList<Camera>& cams, const QList<QImage>& imgs, int i, Point3 x)
   : num(0)
   , cam_i(i)
-  , cameras(&cams)
-  , images(&imgs)
+  , _cameras(&cams)
+  , _images(&imgs)
   {
-    Camera ci = cameras->at(i);
-    QImage img = images->at(i);
+    Camera ci = cams.at(i);
+    QImage img = imgs.at(i);
     Mat4 m = ci.intrinsicForImage(img.width(), img.height());
     txfm_i = m * ci.extrinsic();
 
@@ -38,13 +38,13 @@ struct ClosestCameras {
     if (this->num >= MAX_NUM)
       return false;
 
-    Camera ci = cameras->at(cam_i);
+    Camera ci = _cameras->at(cam_i);
     Vec3 ni = normalize(ci.center() - x);
     int count = this->num;
 
-    for (int j = 0, n = cameras->size(); j < n; ++j) {
-      Camera cj = cameras->at(j);
-      QImage img = images->at(j);
+    for (int j = 0, n = _cameras->size(); j < n; ++j) {
+      Camera cj = _cameras->at(j);
+      QImage img = _images->at(j);
       int w = img.width(), h = img.height();
       Vec3 nj = normalize(cj.center() - x);
       float dp = (float)dot(ni, nj);
@@ -68,7 +68,7 @@ struct VoxelScore1 {
   ClosestCameras ccams;
   SampleWindow swin_i;
   Ray3 ray;
-  QList<QPointF> center_func;
+  QList<QPointF> sjdk;
 
   VoxelScore1(const QList<Camera>& cams,
               const QList<QImage>& imgs,
@@ -80,6 +80,49 @@ struct VoxelScore1 {
     const QImage& image_i = imgs.at(cam_i);
     swin_i = SampleWindow(image_i, Vec3::proj(transform(ccams.txfm_i, x)));
     ray = Ray3(x, normalize(ci.center() - x) * voxel_h * 1.4f);
+
+    sjdk.reserve(128);
+    for (int i = 0; i < ccams.num; ++i) {
+      compute(i);
+    }
+  }
+
+  void compute(int ith_jcam)
+  {
+    int cam_j = ccams.cam_js[ith_jcam];
+    Mat4 txfm_j = ccams.txfm_js[ith_jcam];
+    const QImage& image_j = ccams._images->at(cam_j);
+    int width = image_j.width(), height = image_j.height();
+    Epipolar epipolar(width, height, txfm_j, ray);
+
+    const int bufn = 5;
+    float xbuf[bufn];
+    double ybuf[bufn];
+    int bufi = 0;
+    epipolar.walk(
+      [&xbuf,&ybuf,&bufi,&image_j,this]
+      (float x, float y, float depth, bool inside) {
+        SampleWindow swj(image_j, Vec3(x,y,0.0f));
+        float ncc = NormalizedCrossCorrelation(swin_i, swj);
+        if (__builtin_expect(bufi == (bufn-1), 1)) {
+          xbuf[bufi] = depth;
+          ybuf[bufi] = ncc;
+          bool is_maxima = true;
+          for (int i = 0; i < bufn; ++i) {
+            is_maxima = is_maxima && (ybuf[(bufn/2)] >= ybuf[i]);
+          }
+          if (is_maxima) {
+            sjdk.append(QPointF(xbuf[(bufn/2)], ybuf[(bufn/2)]));
+          }
+          memmove(xbuf, xbuf+1, sizeof(float) * (bufn-1));
+          memmove(ybuf, ybuf+1, sizeof(double) * (bufn-1));
+        } else {
+          xbuf[bufi] = depth;
+          ybuf[bufi] = ncc;
+          bufi++;
+        }
+      }
+    );
   }
 
   /*inline double operator()(float d) const
