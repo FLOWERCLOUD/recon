5b5cc081096204ae2997059ab3c76cad9ffa24db
David Lin <davll.xc@gmail.com>
2015-07-13 17:38:19 +0800

    visualize plane XZ

--- voxel/tools/planexz.py
+++ voxel/tools/planexz.py
@@ -1,42 +1,42 @@
 def parse_args():
     from argparse import ArgumentParser
-    parser = ArgumentParser(description="Visualize Plane XZ of Graph")
+    parser = ArgumentParser(description="Visualize XZ Plane of Graph")
     parser.add_argument('graph', help='Input Graph data file')
     parser.add_argument('--plane-y', type=int)
     return parser.parse_args()
 
-def run(finput):
+def load_graph(path):
     import string, numpy as np
-    global ARGS
-    level = int(finput.readline())
-    width = long(finput.readline())
-    voxel_h = float(finput.readline())
-    length = width * width * width
-    canvas = np.zeros((width, width, width, 3), dtype=np.float32)
-    for i in range(0,length):
-        buf = string.split(finput.readline())
-        x, y, z = map(int, string.split(buf[0][1:-1], ','))
-        frgnd = bool(int(buf[1]))
-    while True:
-        buf = finput.readline()
-        if not buf:
-            break
-        buf = string.split(buf)
-        x, y, z = map(int, string.split(buf[0][1:-1], ','))
-        rho = float(buf[1])
-        if buf[2] == '+x':
-            canvas[y,z,x,0] = rho
-        elif buf[2] == '+y':
-            canvas[y,z,x,1] = rho
-        elif buf[2] == '+z':
-            canvas[y,z,x,2] = rho
+    with open(path, "r") as finput:
+        level = int(finput.readline())
+        width = long(finput.readline())
+        voxel_h = float(finput.readline())
+        finput.readline() # voxel minpos
+        finput.readline() # voxel maxpos
+        canvas = np.zeros((width, width, width, 3), dtype=np.float32)
+        length = width * width * width
+        for i in range(0,length):
+            buf = string.split(finput.readline())
+            x, y, z, frgnd = map(int, buf)
+            frgnd = bool(frgnd)
+        while True:
+            buf = finput.readline()
+            if not buf:
+                break
+            buf = string.split(buf)
+            x, y, z, rho = map(float, buf[0:4])
+            if buf[-1] == '+x':
+                canvas[y,z,x,0] = rho
+            elif buf[-1] == '+y':
+                canvas[y,z,x,1] = rho
+            elif buf[-1] == '+z':
+                canvas[y,z,x,2] = rho
     return canvas
 
 def mainfunc():
     global ARGS
     ARGS = parse_args()
-    with open(ARGS.graph, "r") as finput:
-        image = run(finput)
+    image = load_graph(ARGS.graph)
     import numpy as np
     #image = np.multiply(image, 255, dtype=np.uint8)
     def plot(y):
@@ -54,20 +54,20 @@ def mainfunc():
         Y = np.arange(0, img.shape[0], 1)
         X, Y = np.meshgrid(X, Y)
 
-        fig = plt.figure()
-        plt.suptitle("+x edge")
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot_surface(X, Y, img[:,:,0], rstride=1, cstride=1)
+        #fig = plt.figure()
+        #plt.suptitle("+x edge")
+        #ax = fig.add_subplot(111, projection='3d')
+        #ax.plot_surface(X, Y, img[:,:,0], rstride=1, cstride=1)
 
-        fig = plt.figure()
-        plt.suptitle("+y edge")
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot_surface(X, Y, img[:,:,1], rstride=1, cstride=1)
+        #fig = plt.figure()
+        #plt.suptitle("+y edge")
+        #ax = fig.add_subplot(111, projection='3d')
+        #ax.plot_surface(X, Y, img[:,:,1], rstride=1, cstride=1)
 
-        fig = plt.figure()
-        plt.suptitle("+z edge")
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot_surface(X, Y, img[:,:,2], rstride=1, cstride=1)
+        #fig = plt.figure()
+        #plt.suptitle("+z edge")
+        #ax = fig.add_subplot(111, projection='3d')
+        #ax.plot_surface(X, Y, img[:,:,2], rstride=1, cstride=1)
 
         fig = plt.figure()
         plt.suptitle("magtitude")
